d95f8143cf49b12dc801c4a4f9e3920b1f9fff63
dharanikuppala <137318054+dharanikuppala@users.noreply.github.com>
2026-03-14 17:42:43 +0530

    Create 04_reverse_string.java

---
+++ 04-strings/04_reverse_string.java
@@ -0,0 +1,32 @@
+/*
+Problem: 344. Reverse String
+Platform: LeetCode
+Difficulty: Easy
+Pattern: Two Pointers
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public void reverseString(char[] s) {
+
+        if(s.length <= 1){
+            return;
+        }
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left < right){
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
