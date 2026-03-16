b0831de68933a0c2ae91f727534a50f00d504f23
dharanikuppala <137318054+dharanikuppala@users.noreply.github.com>
2026-03-16 21:38:39 +0530

    Added solution for 70 climbing_stairs.java

---
+++ 11-dynamic_programming/01_climbing_stairs.java
@@ -0,0 +1,30 @@
+/*
+Problem: 70. Climbing Stairs
+Platform: LeetCode
+Difficulty: Easy
+Pattern: Dynamic Programming / Fibonacci
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+
+    public int climbStairs(int n) {
+
+        if(n <= 1) return n;
+
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for(int i = 2; i <= n; i++){
+
+            int curr = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
